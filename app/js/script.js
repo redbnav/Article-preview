acf968aa3daca573b1c1b53df4c94eb975d523ac
Naveen Reddy Bandakunta <redbnav@Naveens-MacBook-Pro-2.local>
2021-03-08 14:33:05 -0600

    desktop popover

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,10 @@
 const shareBtn = document.querySelector(".btn-share");
+const socialMedia = document.querySelector(".social-media");
 
-shareBtn.addEventListener("click", () => {
-  console.log("clicked");
+shareBtn.addEventListener("click", (e) => {
+  if (socialMedia.classList.contains("active")) {
+    socialMedia.classList.remove("active");
+  } else {
+    socialMedia.classList.add("active");
+  }
 });
